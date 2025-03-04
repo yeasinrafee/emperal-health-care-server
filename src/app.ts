89bc894b169ccf6fe74d546d382777b92ee595bf
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-03-04 23:44:15 +0600

    Admin creation with moduler pattern

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { userRoutes } from './app/modules/User/user.routes';
 
 const app: Application = express();
 
 app.use(cors());
+app.use('/api/v1/user', userRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send({
---
+++ src/app/modules/User/user.service.ts
@@ -0,0 +1,9 @@
+const createAdmin = async () => {
+  return {
+    message: 'admin created',
+  };
+};
+
+export const userService = {
+  createAdmin,
+};
