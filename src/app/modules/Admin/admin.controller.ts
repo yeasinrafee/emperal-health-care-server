ea3fc43e0180981e24d2c491157969249dd7fecd
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-03-23 01:09:14 +0600

    Implementing search and filter function using serchTerm query

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -2,12 +2,20 @@ import { Request, Response } from 'express';
 import { AdminService } from './admin.service';
 
 const getAllAdminFromDB = async (req: Request, res: Response) => {
-  const result = await AdminService.getAllAdminFromDB();
-  res.status(200).json({
-    success: true,
-    message: 'Admins retrieved successfully',
-    data: result,
-  });
+  try {
+    const result = await AdminService.getAllAdminFromDB(req.query);
+    res.status(200).json({
+      success: true,
+      message: 'Admins retrieved successfully',
+      data: result,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err?.name || 'Something went wrong!',
+      error: err,
+    });
+  }
 };
 
 export const AdminController = {
--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -2,8 +2,32 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const getAllAdminFromDB = async () => {
-  const result = await prisma.admin.findMany();
+const getAllAdminFromDB = async (params: any) => {
+  console.log(params);
+  const result = await prisma.admin.findMany({
+    where: {
+      OR: [
+        {
+          name: {
+            contains: params.searchTerm,
+            mode: 'insensitive',
+          },
+        },
+        {
+          email: {
+            contains: params.searchTerm,
+            mode: 'insensitive',
+          },
+        },
+        {
+          contactNumber: {
+            contains: params.searchTerm,
+            mode: 'insensitive',
+          },
+        },
+      ],
+    },
+  });
   return result;
 };
 
