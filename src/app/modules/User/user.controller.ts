9a894128ccf1aba5ff7a6fc89737348b54fa80bb
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-03-24 22:46:21 +0600

    User controller updated

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,22 +1,18 @@
-import { Request, Response } from 'express';
+import { RequestHandler } from 'express';
 import { userService } from './user.service';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import status from 'http-status';
 
-const createAdmin = async (req: Request, res: Response) => {
-  try {
-    const result = await userService.createAdmin(req.body);
-    res.status(200).json({
-      success: true,
-      message: 'Admin created successfully',
-      data: result,
-    });
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: err?.name || 'Something went wrong',
-      error: err,
-    });
-  }
-};
+const createAdmin: RequestHandler = catchAsync(async (req, res) => {
+  const result = await userService.createAdmin(req.body);
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: 'Admin created successfully',
+    data: result,
+  });
+});
 
 export const userController = {
   createAdmin,
