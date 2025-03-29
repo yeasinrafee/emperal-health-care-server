244590c1cf46cea6d40390019c18737b928a565a
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-03-29 11:15:59 +0600

    taking file using multer

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import { UserController } from './user.controller';
 import auth from '../../middlewares/auth';
 import { UserRole } from '@prisma/client';
+import { fileUploader } from '../../../helpers/fileUploader';
 
 const router = express.Router();
 
 router.post(
   '/',
   auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  fileUploader.upload.single('file'),
   UserController.createAdmin
 );
 
