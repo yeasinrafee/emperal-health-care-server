6dd453577bd9168197347cfdf4ad731b160e2bd0
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-04-20 01:49:23 +0600

    create prescription validation

--- src/app/modules/Prescription/prescription.routes.ts
+++ src/app/modules/Prescription/prescription.routes.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import auth from '../../middlewares/auth';
 import { PrescriptionController } from './prescription.controller';
 import { UserRole } from '@prisma/client';
+import validateRequest from '../../middlewares/validateRequest';
+import { PrescriptionValidation } from './prescription.validation';
 
 const router = express.Router();
 
@@ -20,6 +22,7 @@ router.get(
 router.post(
   '/',
   auth(UserRole.DOCTOR),
+  validateRequest(PrescriptionValidation.createPrescriptionZodValidation),
   PrescriptionController.createPrescriptionIntoDB
 );
 
