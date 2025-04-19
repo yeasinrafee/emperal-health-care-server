b76e8777a3dfb3a3b1910a9a2cc77ad96e0695ab
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-04-20 01:41:19 +0600

    Create review validation

--- src/app/modules/Review/review.routes.ts
+++ src/app/modules/Review/review.routes.ts
@@ -2,11 +2,18 @@ import express from 'express';
 import auth from '../../middlewares/auth';
 import { UserRole } from '@prisma/client';
 import { ReviewController } from './review.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { ReviewValidation } from './review.validation';
 
 const router = express.Router();
 
 router.get('/', ReviewController.getAllReviewsFromDB);
 
-router.post('/', auth(UserRole.PATIENT), ReviewController.createReviewIntoDB);
+router.post(
+  '/',
+  auth(UserRole.PATIENT),
+  validateRequest(ReviewValidation.createReviewZodValidation),
+  ReviewController.createReviewIntoDB
+);
 
 export const ReviewRoutes = router;
