ddeaab19699bb0296d480bef0002e7282924cc36
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-04-19 19:05:07 +0600

    Payment initialized successfully

--- src/app/modules/Payment/payment.service.ts
+++ src/app/modules/Payment/payment.service.ts
@@ -1,6 +1,5 @@
-import axios from 'axios';
-import config from '../../../config';
 import prisma from '../../../shared/prisma';
+import { SSLService } from '../SSL/ssl.service';
 
 // 1. Initiate Payment
 const initPayment = async (appointmentId: string) => {
@@ -17,51 +16,20 @@ const initPayment = async (appointmentId: string) => {
     },
   });
 
-  console.log(paymentData);
-
-  const data = {
-    store_id: config.ssl.storeId,
-    store_passwd: config.ssl.storePass,
-    total_amount: paymentData.amount,
-    currency: 'BDT',
-    tran_id: paymentData.transactionId, // use unique tran_id for each api call
-    success_url: config.ssl.successUrl,
-    fail_url: config.ssl.failUrl,
-    cancel_url: config.ssl.cancelUrl,
-    ipn_url: 'http://localhost:3030/ipn',
-    shipping_method: 'N/A',
-    product_name: 'Appointment Payment',
-    product_category: 'Service',
-    product_profile: 'general',
-    cus_name: paymentData.appointment.patient.name,
-    cus_email: paymentData.appointment.patient.email,
-    cus_add1: paymentData.appointment.patient.address,
-    cus_add2: 'N/A',
-    cus_city: 'Dhaka',
-    cus_state: 'Dhaka',
-    cus_postcode: '1000',
-    cus_country: 'Bangladesh',
-    cus_phone: paymentData.appointment.patient.contactNumber,
-    cus_fax: '01711111111',
-    ship_name: 'N/A',
-    ship_add1: 'N/A',
-    ship_add2: 'N/A',
-    ship_city: 'N/A',
-    ship_state: 'N/A',
-    ship_postcode: 1000,
-    ship_country: 'N/A',
+  const initPaymentData = {
+    amount: paymentData.amount,
+    transactionId: paymentData.transactionId,
+    name: paymentData.appointment.patient.name,
+    email: paymentData.appointment.patient.email,
+    address: paymentData.appointment.patient.address,
+    phoneNumber: paymentData.appointment.patient.contactNumber,
   };
 
-  const response = await axios({
-    method: 'post',
-    url: config.ssl.sslPaymentApi,
-    data: data,
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  });
+  const result = await SSLService.initPayment(initPaymentData);
 
-  console.log(response.data);
+  return {
+    paymentUrl: result.GatewayPageURL,
+  };
 };
 
 export const PaymentService = {
---
+++ src/app/modules/SSL/ssl.service.ts
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import config from '../../../config';
+import ApiError from '../../errors/ApiError';
+import status from 'http-status';
+
+const initPayment = async (paymentData: any) => {
+  try {
+    const data = {
+      store_id: config.ssl.storeId,
+      store_passwd: config.ssl.storePass,
+      total_amount: paymentData.amount,
+      currency: 'BDT',
+      tran_id: paymentData.transactionId, // use unique tran_id for each api call
+      success_url: config.ssl.successUrl,
+      fail_url: config.ssl.failUrl,
+      cancel_url: config.ssl.cancelUrl,
+      ipn_url: 'http://localhost:3030/ipn',
+      shipping_method: 'N/A',
+      product_name: 'Appointment Payment',
+      product_category: 'Service',
+      product_profile: 'general',
+      cus_name: paymentData.name,
+      cus_email: paymentData.email,
+      cus_add1: paymentData.address,
+      cus_add2: 'N/A',
+      cus_city: 'Dhaka',
+      cus_state: 'Dhaka',
+      cus_postcode: '1000',
+      cus_country: 'Bangladesh',
+      cus_phone: paymentData.phoneNumber,
+      cus_fax: '01711111111',
+      ship_name: 'N/A',
+      ship_add1: 'N/A',
+      ship_add2: 'N/A',
+      ship_city: 'N/A',
+      ship_state: 'N/A',
+      ship_postcode: 1000,
+      ship_country: 'N/A',
+    };
+
+    const response = await axios({
+      method: 'post',
+      url: config.ssl.sslPaymentApi,
+      data: data,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+
+    return response.data;
+  } catch (err) {
+    throw new ApiError(status.BAD_REQUEST, 'Payment error occurred.');
+  }
+};
+
+export const SSLService = {
+  initPayment,
+};
