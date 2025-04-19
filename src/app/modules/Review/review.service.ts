3f3111c00d92bd4905a017d7cd9ac37cd2c6956c
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-04-20 00:20:33 +0600

    creating ratings

--- src/app/modules/Review/review.service.ts
+++ src/app/modules/Review/review.service.ts
@@ -1,7 +1,38 @@
+import status from 'http-status';
+import prisma from '../../../shared/prisma';
+import ApiError from '../../errors/ApiError';
 import { TAuthUser } from '../../types/common';
 
 // 1. Create Review
-const createReviewIntoDB = async (user: TAuthUser, payload: any) => {};
+const createReviewIntoDB = async (user: TAuthUser, payload: any) => {
+  const patientData = await prisma.patient.findUniqueOrThrow({
+    where: {
+      email: user?.email,
+    },
+  });
+
+  const appointmentData = await prisma.appointment.findUniqueOrThrow({
+    where: {
+      id: payload.appointmentId,
+    },
+  });
+
+  if (patientData.id !== appointmentData.patientId) {
+    throw new ApiError(status.BAD_REQUEST, 'This is not your appointment!');
+  }
+
+  const result = await prisma.review.create({
+    data: {
+      appointmentId: appointmentData.id,
+      doctorId: appointmentData.doctorId,
+      patientId: appointmentData.patientId,
+      rating: payload.rating,
+      comment: payload.comment,
+    },
+  });
+
+  return result;
+};
 
 export const ReviewService = {
   createReviewIntoDB,
