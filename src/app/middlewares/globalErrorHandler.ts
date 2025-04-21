cc7a1dab8c2e7552e07702edc83392faa93271ea
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-04-22 00:12:07 +0600

    Global Error modified

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,3 +1,4 @@
+import { Prisma } from '@prisma/client';
 import { NextFunction, Request, Response } from 'express';
 import status from 'http-status';
 
@@ -7,10 +8,25 @@ const globalErrorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  res.status(status.INTERNAL_SERVER_ERROR).json({
-    success: false,
-    message: err?.message || 'Something went wrong!',
-    error: err,
+  let statusCode = status.INTERNAL_SERVER_ERROR;
+  let success = false;
+  let message = err?.message || 'Something went wrong!';
+  let error = err;
+
+  if (err instanceof Prisma.PrismaClientValidationError) {
+    message = 'Validation Error!';
+    error = err.message;
+  } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === 'P2002') {
+      message = 'Duplicate Key Error!';
+      error = err.meta;
+    }
+  }
+
+  res.status(statusCode).json({
+    success,
+    message,
+    error,
   });
 };
 
