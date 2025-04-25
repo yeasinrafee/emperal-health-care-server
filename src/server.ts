080036798d0b6b036d4c989b2f8c0581f942a3f1
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-04-25 16:48:44 +0600

    Handling uncaughtExeption and UnhandledRejection Errors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,29 @@
+import { Server } from 'http';
 import app from './app';
 import config from './config';
 
 async function main() {
-  app.listen(config.port, () => {
+  const server: Server = app.listen(config.port, () => {
     console.log('Server is running on port: ', config.port);
+  });
+
+  const exitHandler = () => {
+    if (server) {
+      server.close(() => {
+        console.log('Server is closed!!');
+      });
+    }
+    process.exit(1);
+  };
+
+  process.on('uncaughtException', (err) => {
+    console.log(err);
+    exitHandler();
+  });
+
+  process.on('unhandledRejection', (err) => {
+    console.log(err);
+    exitHandler();
   });
 }
 main();
