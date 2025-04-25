32ffd58b4a82fed90dd822f814fcc7aa642cc39d
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-04-25 16:43:03 +0600

    Enabling Prisma logging

--- src/shared/prisma.ts
+++ src/shared/prisma.ts
@@ -1,5 +1,42 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: [
+    // {
+    //   emit: 'event',
+    //   level: 'query',
+    // },
+    {
+      emit: 'event',
+      level: 'error',
+    },
+    {
+      emit: 'event',
+      level: 'info',
+    },
+    {
+      emit: 'event',
+      level: 'warn',
+    },
+  ],
+});
+
+prisma.$on('warn', (e) => {
+  console.log(e);
+});
+
+prisma.$on('info', (e) => {
+  console.log(e);
+});
+
+prisma.$on('error', (e) => {
+  console.log(e);
+});
+
+// prisma.$on('query', (e) => {
+//   console.log('Query: ' + e.query);
+//   console.log('Params: ' + e.params);
+//   console.log('Duration: ' + e.duration + 'ms');
+// });
 
 export default prisma;
